a8c25495b312a844eb2a165f66870af83a8333e7
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2019-11-11 16:29:21 +0100

    Add units to 3c_273

--- figs/3c_273.py
+++ figs/3c_273.py
@@ -9,8 +9,10 @@ rc("text", usetex=True)
 
 hdul = fits.open("data/spec-0519-52283-0021.fits")
 data = hdul[1].data
-title = "Quasar 3C 273"
 
-ax = plt.axes(xlabel="wavelength", title=title)
+xlabel = "Wavelength (\AA{})"
+ylabel = "$f_{\lambda}$ (10$^{-17}$ erg s$^{-1}$ cm$^{-2}$ \AA{}$^{-1}$)"
+
+ax = plt.axes(xlabel=xlabel, ylabel=ylabel)
 ax.plot(np.power(10, data["loglam"]), data["flux"])
 plt.savefig("3c_273.pdf")
