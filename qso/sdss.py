2e21a1a2d3195787a8e78029e11801a209d83753
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2019-11-20 15:51:30 +0100

    Implement code for SDSS to design matrices

--- qso/sdss.py
+++ qso/sdss.py
@@ -1,5 +1,29 @@
 from os import path
 
+import pandas as pd
+
+
+def read_selected_catalog(filepath):
+    return pd.read_csv(
+        filepath,
+        index_col=["plate", "mjd", "fiberid"],
+        dtype={
+            "platequality": "category",
+            "targettype": "category",
+            "wavemax": "f4",
+            "wavemin": "f4",
+            "zwarning": "i4"
+            },
+        )
+
+
+def get_filename(plate, mjd, fiberid):
+    return "spec-{:04d}-{:05d}-{:04d}.fits".format(plate, mjd, fiberid)
+
+
+def get_dr_path(plate, mjd, fiberid):
+    return path.join("{:04d}".format(plate), get_filename(plate, mjd, fiberid))
+
 
 def get_spec_filename(spec):
     plate = spec["plate"]
