486eea621828e25343d99885f03d1d97f7ecacce
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2019-12-18 09:17:26 +0100

    Add weights initialisation

--- qso/utils.py
+++ qso/utils.py
@@ -4,3 +4,12 @@ import numpy as np
 WAVEMIN, WAVEMAX = 3.5843, 3.9501
 N_WAVELENGTHS = 3659
 WAVES = np.logspace(WAVEMIN, WAVEMAX, N_WAVELENGTHS)
+
+
+def init_weights(m):
+    if type(m) == nn.Conv1d:
+        nn.init.xavier_uniform_(m.weight.data)
+        m.bias.data.fill_(0)
+    elif type(m) == nn.Linear:
+        nn.init.xavier_uniform_(m.weight.data)
+        m.bias.data.fill_(0)
